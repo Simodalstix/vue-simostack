fc8ead84b3447844286dd4f1bf6b0959cdc969f7
Simon Parker <lastpolar@gmail.com>
2025-05-28 14:33:35 +1000

    Fixed wording for python badge modal

--- src/components/modals/PythonModal.vue
+++ src/components/modals/PythonModal.vue
@@ -14,11 +14,12 @@ PCAP™ – Certified Associate Python Programmer"
     ><div class="bg-gray-700 p-3">
       <p class="text-sm text-gray-300 mb-2">Issued by Python Institute</p>
       <p class="text-sm text-gray-300">
-        Earners of this certification have a comprehensive understanding of application life-cycle
-        management. They demonstrated proficiency in writing applications using AWS service APIs,
-        AWS CLI, and SDKs; using containers; and deploying with a CI/CD pipeline. Certification
-        holders can develop, deploy, and debug cloud-based applications following AWS best
-        practices.
+        Earners of the PCAP – Certified Associate Python Programmer badge demonstrate the ability to
+        accomplish coding tasks related to the basics of programming in the Python language and the
+        fundamental notions and techniques used in object-oriented programming. Becoming PCAP
+        certified ensures that the individual is fully acquainted with all the primary means
+        provided by Python 3 to enable him to start his own studies, and to open a path to the
+        developer’s career.
         <a
           href="https://www.credly.com/badges/dd32e128-d849-40fa-b39c-e4b77b3480cd"
           target="_blank"
