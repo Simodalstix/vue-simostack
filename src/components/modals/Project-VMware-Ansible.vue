97e0d67223554c9addab3d0468e9b4ef7fe8c8f4
Simon Parker <lastpolar@gmail.com>
2025-06-10 16:14:11 +1000

    fix

--- src/components/modals/Project-VMware-Ansible.vue
+++ src/components/modals/Project-VMware-Ansible.vue
@@ -1,6 +1,6 @@
 <template>
   <BaseModal :visible="visible" @close="$emit('close')" title="VMware + Monitoring Lab">
-    <div class="grid grid-cols-1 md:grid-cols-[5fr,2fr] mx-3 gap-6 text-gray-200">
+    <div class="grid grid-cols-1 md:grid-cols-[5fr,2fr] mx-3 gap-5 text-gray-200">
       <!-- Left: Diagram + summary -->
       <div class="mt-3">
         <img
