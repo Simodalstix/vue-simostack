cf256b20251e9de26d8d88c2c0dbb0804021e4aa
Simon Parker <lastpolar@gmail.com>
2025-06-15 11:59:20 +1000

    updated Dynatrace project modal and card

--- src/components/modals/Project-Dynatrace.vue
+++ src/components/modals/Project-Dynatrace.vue
@@ -2,35 +2,33 @@
   <BaseModal
     :visible="visible"
     @close="$emit('close')"
-    title="Azure IaaS Operations lab (Coming soon, give me a sec)"
+    title="Azure Patch Management & Observability Lab"
   >
     <div class="grid grid-cols-1 md:grid-cols-[5fr,2fr] mx-3 gap-6 text-gray-200">
       <div class="mt-3">
         <img
-          src="/images/project-modal-images/azure-console.png"
+          src="/images/project-modal-images/dynatrace-console.png"
           alt="Azure IaaS Operations Diagram"
           class="object-contain rounded shadow"
         />
 
         <p class="italic text-base text-orange-300 mt-3">
           A robust Azure IaaS project demonstrating automated VM provisioning, patching, and
-          comprehensive observability. (Coming soon) I'll start drawing my Diagram either Friday or
-          Saturday - using Excalidraw
+          comprehensive observability.
         </p>
       </div>
 
       <div class="text-base space-y-4 mt-3">
         <p>
-          I'm currently building an Azure-based infrastructure platform using Terraform. This lab
-          focuses on provisioning highly available RHEL and Windows Server VMs within a secure
-          network architecture, including Load Balancers and Availability Sets for resilience.
+          I recently built an Azure-based infrastructure platform using Terraform. This lab
+          provisions highly available RHEL and Windows Server VMs within a secure network
+          architecture, leveraging Load Balancers and Availability Sets for resilience.
         </p>
         <p>
-          Key areas include automated patching processes leveraging Azure Update Management Center,
-          granular RBAC, and policy enforcement. For comprehensive observability, I'm integrating
-          native Azure Monitor (Log Analytics) with Dynatrace OneAgent deployments. This project
-          strengthens my skills in IaC, cloud operations, security, and advanced monitoring
-          practices.
+          Key components include automated patching via Azure Update Management Center, granular
+          RBAC, and policy enforcement. For observability, I integrated Azure Monitor (Log
+          Analytics) alongside Dynatrace OneAgent deployments. This project solidified my skills in
+          IaC, cloud operations, security, and advanced monitoring practices.
         </p>
         <div class="flex gap-3 items-center">
           <!-- <img src="/images/main-tools/terraform.svg" class="h-24" title="Terraform" />
